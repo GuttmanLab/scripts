a3f6ba2adde3ebccf09a5741a860cbada60eb657
Mason Lai <mason@ip-172-31-3-17.us-west-2.compute.internal>
2016-11-04 23:30:47 +0000

    shred program truncates read names like bowtie2

--- sequence/shred_fasta_into_kmers.py
+++ sequence/shred_fasta_into_kmers.py
@@ -6,15 +6,15 @@ def main():
 
 class FastaRecord:
     def __init__(self, name, sequence):
-        self.name = name
+        self.name = name.split()[0]  # Remove all characters after first whitespace
         self.sequence = sequence
 
-    def shred(self, kmer_size):
+    def shred(self, kmer_size, discard_too_short):
         kmers = []
         too_short = len(self.sequence) < kmer_size
-        if too_short:
+        if too_short and not discard_too_short:
             kmers.append(FastaRecord(self.name + ":0-" + str(len(self.sequence)), self.sequence))
-        else:
+        elif not too_short:
             for start_coord in range(len(self.sequence) - kmer_size + 1):
                 end_coord = start_coord + kmer_size
                 subname = self.name + ":" + str(start_coord) + "-" + str(end_coord)
@@ -25,14 +25,13 @@ class FastaRecord:
     def append(self, additional_sequence):
         self.sequence += additional_sequence
 
-    def shred_and_print(self, kmer_size):
-        kmers = self.shred(kmer_size)
-        for kmer in kmers:
-            kmer.print()
-
     def print(self):
         print(self.name)
         print(self.sequence)
+
+def print_kmers(kmers):
+    for kmer in kmers:
+        kmer.print()
 
 def shred_fastas_and_print(args):
 
@@ -42,14 +41,16 @@ def shred_fastas_and_print(args):
         for line in input_file:
             if line.startswith(">"):
                 if current_fasta:
-                    current_fasta.shred_and_print(args.kmer_size)
+                    kmers = current_fasta.shred(args.kmer_size, args.discard_too_short)
+                    print_kmers(kmers)
                 current_fasta = FastaRecord(line.rstrip(), "")
             else:
                 current_fasta.append(line.rstrip())
 
         # shred the last sequence in the file
         if current_fasta:
-            current_fasta.shred_and_print(args.kmer_size)
+            kmers = current_fasta.shred(args.kmer_size, args.discard_too_short)
+            print_kmers(kmers)
 
 def parse_arguments():
 
@@ -58,7 +59,7 @@ def parse_arguments():
                         action = "store")
     parser.add_argument("-k", "--size", help = "The size of the k-mers",
                         action = "store", type = int, default = 35, dest = "kmer_size")
-    parser.add_argument("-d", "--discard", action = "store_true",
+    parser.add_argument("-d", "--discard", action = "store_true", dest = "discard_too_short",
                         help = "Discards FASTA records if length is less than k-mer size")
     return parser.parse_args()
 
